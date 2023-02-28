2e3e8e21232b821cd6404b47722ad2c17062fec7
OlekSytn <oleksandr.sytnikov0413@gmail.com>
2023-02-28 06:05:50 -0800

    2 pm

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
+from . import views
+
+app_name = 'polls'
 urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
